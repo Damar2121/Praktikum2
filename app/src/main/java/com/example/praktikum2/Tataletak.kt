0eced07197db89c4d39b308bf88242573c773267
Damar2121 <damar.vivoy51@gmail.com>
2025-10-16 17:00:22 +0700

    nambah Tataletak dengan spacer dan image

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -1,5 +1,7 @@
 package com.example.praktikum2
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,7 +20,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 
 
 @Composable
@@ -126,6 +131,26 @@ fun TataletakBoxColoumnRow(modifier: Modifier) {
                     Text(text = "Col1_Row2_Komponen")
                 }
             }
+        }
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+            Text(text = "My music",
+                fontsize = 50.sp,
+                color = Color.Red,
+                fontweight = FontWeight.Bold,
+            fontFamily = FontFamily.Cursive,
+            modifier = Modifier.align(
+                alignment = Alignment.Center))
+
         }
     }
 }
